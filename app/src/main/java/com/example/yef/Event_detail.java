b467df3e0b318b983c5f65353ad04c3a480b7327
Debojyoti Sarkar <debu.athletico@gmail.com>
2019-10-12 21:19:51 +0530

    Resolved Several Errors

---
+++ app/src/main/java/com/example/yef/Event_detail.java
@@ -0,0 +1,13 @@
+package com.example.yef;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Event_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_detail);
+    }
+}
