ae0ebf75ece2c040b17e229a9c7b131a263ae24f
Shrini <manjithayas@gmail.com>
2019-10-12 08:50:22 +0530

    Started UI Design

--- app/src/main/java/com/example/yef/MainActivity.java
+++ app/src/main/java/com/example/yef/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.yef;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView titlehome = findViewById(R.id.YEF_text);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getToHome(View view){
+//        Function to get to home page activity on title text click
     }
 }
